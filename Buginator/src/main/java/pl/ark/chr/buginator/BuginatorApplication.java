57ee2f9f9265193af9482a52ee1a585a5fda5020
Arkadiusz Chrobot <chrobot.arkadiusz@gmail.com>
2016-09-25 20:12:05 +0200

    Basic configuration

---
+++ Buginator/src/main/java/pl/ark/chr/buginator/BuginatorApplication.java
@@ -0,0 +1,12 @@
+package pl.ark.chr.buginator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuginatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuginatorApplication.class, args);
+	}
+}
