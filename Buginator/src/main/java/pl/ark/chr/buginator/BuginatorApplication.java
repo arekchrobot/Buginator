aeea07e79a0a8500abd715e4d0323c21d3086d5a
Arkadiusz Chrobot <chrobot.arkadiusz@gmail.com>
2016-09-29 14:31:18 +0200

    setting up https openshift

--- Buginator/src/main/java/pl/ark/chr/buginator/BuginatorApplication.java
+++ Buginator/src/main/java/pl/ark/chr/buginator/BuginatorApplication.java
@@ -9,11 +9,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class BuginatorApplication {
 
 	@Bean
+	@Profile("dev")
 	public EmbeddedServletContainerFactory servletContainer() {
 		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
 			@Override
@@ -31,12 +33,41 @@ public class BuginatorApplication {
 		return tomcat;
 	}
 
+	@Bean
+	@Profile("openshift")
+	public EmbeddedServletContainerFactory servletContainerOpenshift() {
+		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+
+		tomcat.addAdditionalTomcatConnectors(initiateHttpConnectorOpenishft());
+		return tomcat;
+	}
+
 	private Connector initiateHttpConnector() {
 		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
 		connector.setScheme("http");
 		connector.setPort(8080);
 		connector.setSecure(false);
 		connector.setRedirectPort(8443);
+
+		return connector;
+	}
+
+	private Connector initiateHttpConnectorOpenishft() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setScheme("http");
+		connector.setPort(8080);
+		connector.setSecure(false);
+		connector.setRedirectPort(443);
 
 		return connector;
 	}
