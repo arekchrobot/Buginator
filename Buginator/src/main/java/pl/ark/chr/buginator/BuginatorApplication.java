6fc3095ee85848b0aa7d9b80af668afa6c7ec8e1
Arkadiusz Chrobot <chrobot.arkadiusz@gmail.com>
2016-09-29 14:35:43 +0200

    removing tomcat ssl port

--- Buginator/src/main/java/pl/ark/chr/buginator/BuginatorApplication.java
+++ Buginator/src/main/java/pl/ark/chr/buginator/BuginatorApplication.java
@@ -33,41 +33,12 @@ public class BuginatorApplication {
 		return tomcat;
 	}
 
-	@Bean
-	@Profile("openshift")
-	public EmbeddedServletContainerFactory servletContainerOpenshift() {
-		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
-			@Override
-			protected void postProcessContext(Context context) {
-				SecurityConstraint securityConstraint = new SecurityConstraint();
-				securityConstraint.setUserConstraint("CONFIDENTIAL");
-				SecurityCollection collection = new SecurityCollection();
-				collection.addPattern("/*");
-				securityConstraint.addCollection(collection);
-				context.addConstraint(securityConstraint);
-			}
-		};
-
-		tomcat.addAdditionalTomcatConnectors(initiateHttpConnectorOpenishft());
-		return tomcat;
-	}
-
 	private Connector initiateHttpConnector() {
 		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
 		connector.setScheme("http");
 		connector.setPort(8080);
 		connector.setSecure(false);
 		connector.setRedirectPort(8443);
-
-		return connector;
-	}
-
-	private Connector initiateHttpConnectorOpenishft() {
-		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-		connector.setScheme("http");
-		connector.setPort(8080);
-		connector.setSecure(false);
-		connector.setRedirectPort(443);
 
 		return connector;
 	}
