5002631e749dcf2441481e8b983db2a2d01531d4
Arkadiusz Chrobot <chrobot.arkadiusz@gmail.com>
2016-10-06 22:56:20 +0200

    added login page

--- Buginator/src/main/resources/static/js/app/auth/auth.controller.js
+++ Buginator/src/main/resources/static/js/app/auth/auth.controller.js
@@ -1,45 +1,51 @@
-angular.module("buginator.authController", [])
-    .controller("authController", function ($rootScope, $scope, $state, authService, $location) {
-        $scope.credentials = {};
-        $rootScope.loginError = false;
+angular.module("buginator.authController", []).config(function ($stateProvider) {
+    $stateProvider.state("login", {
+        url: "/login",
+        templateUrl: "html/auth/login.html"
+    });
+}).controller("authController", function ($rootScope, $scope, $state, authService, $location) {
+    $scope.credentials = {};
+    $rootScope.loginError = false;
 
-        $scope.authenticate = function (credentials) {
-            authService.authenticate(credentials,
-                function (returnedData) {
-                    $rootScope.user = returnedData;
-                    $rootScope.loginError = false;
-                    $rootScope.user.perms = authService.createPermissions($rootScope.user);
-                    $scope.credentials = {};
-                }, function (returnedData) {
-                    $scope.credentials = {};
-                    $rootScope.loginError = true;
-                });
-        };
-
-        $scope.isActive = function (viewLocation) {
-            return viewLocation === $location.path();
-        };
-
-        $scope.login = function () {
-            $scope.authenticate($scope.credentials);
-        };
-
-        authService.isLogged(
+    $scope.authenticate = function (credentials) {
+        authService.authenticate(credentials,
             function (returnedData) {
                 $rootScope.user = returnedData;
+                $rootScope.loginError = false;
                 $rootScope.user.perms = authService.createPermissions($rootScope.user);
+                $scope.credentials = {};
+            }, function (returnedData) {
+                $scope.credentials = {};
+                $rootScope.loginError = true;
+            });
+    };
+
+    $rootScope.$state = $state;
+
+    $scope.isActive = function (viewLocation) {
+        return viewLocation === $location.path();
+    };
+
+    $scope.login = function () {
+        $scope.authenticate($scope.credentials);
+    };
+
+    authService.isLogged(
+        function (returnedData) {
+            $rootScope.user = returnedData;
+            $rootScope.user.perms = authService.createPermissions($rootScope.user);
+        }, function (returnedData) {
+            $rootScope.user = null;
+        });
+
+    $scope.logout = function () {
+        authService.logout(
+            function (returnedData) {
+                $rootScope.user = null;
+                $state.go("login");
             }, function (returnedData) {
                 $rootScope.user = null;
+                $state.go("login");
             });
-
-        $scope.logout = function () {
-            authService.logout(
-                function (returnedData) {
-                    $rootScope.user = null;
-                    //$state.go("login");
-                }, function (returnedData) {
-                    $rootScope.user = null;
-                    //$state.go("login");
-                });
-        };
-    });
+    };
+});
