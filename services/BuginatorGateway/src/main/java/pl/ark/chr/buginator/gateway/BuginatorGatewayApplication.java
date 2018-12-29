07b71bfdaf744b995d7d4bf2fa721456a5dc00ca
Arek Chrobot <chrobot.arkadiusz@gmail.com>
2018-12-29 22:41:52 +0100

    Added Gateway service for easier frontend communication

---
+++ services/BuginatorGateway/src/main/java/pl/ark/chr/buginator/gateway/BuginatorGatewayApplication.java
@@ -0,0 +1,12 @@
+package pl.ark.chr.buginator.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuginatorGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuginatorGatewayApplication.class, args);
+    }
+}
