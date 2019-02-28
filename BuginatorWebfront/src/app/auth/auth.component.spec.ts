99113e43c17604ec6052e400230c461ea762cf16
Arek Chrobot <chrobot.arkadiusz@gmail.com>
2019-02-28 23:02:43 +0100

    added tests for auth component

--- BuginatorWebfront/src/app/auth/auth.component.spec.ts
+++ BuginatorWebfront/src/app/auth/auth.component.spec.ts
@@ -1,6 +1,15 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { AuthComponent } from './auth.component';
+import {AuthComponent} from './auth.component';
+import {RouterTestingModule} from "@angular/router/testing";
+import {TranslateTestingModule} from "ngx-translate-testing";
+import {ReactiveFormsModule} from "@angular/forms";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {LoginComponent} from "./login/login.component";
+import {PasswordResetComponent} from "./password-reset/password-reset.component";
+import {RegisterComponent} from "./register/register.component";
+import {CookieService} from "ngx-cookie-service";
+import {By} from "@angular/platform-browser";
 
 describe('AuthComponent', () => {
   let component: AuthComponent;
@@ -8,9 +17,23 @@ describe('AuthComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AuthComponent ]
-    })
-    .compileComponents();
+      imports: [
+        RouterTestingModule,
+        TranslateTestingModule
+          .withTranslations('en', require('../../assets/i18n/en.json')),
+        ReactiveFormsModule,
+        HttpClientTestingModule
+      ],
+      declarations: [
+        AuthComponent,
+        LoginComponent,
+        PasswordResetComponent,
+        RegisterComponent
+      ],
+      providers: [
+        CookieService
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +42,54 @@ describe('AuthComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create and set body class', () => {
     expect(component).toBeTruthy();
+    expect(document.body.classList.contains('login')).toBeTruthy();
+
+    expect(component.activeTab).toBe('login');
+
+    const elements = fixture.debugElement.queryAll(By.css('.active'));
+    expect(elements.length).toBe(1);
+
+    let expectedDiv = elements[0].nativeElement;
+    expect(expectedDiv.id).toBe('login');
+  });
+
+  it('should remove document body class on destroy', () => {
+    //when
+    fixture.destroy();
+
+    //then
+    expect(document.body.classList.contains('login')).toBeFalsy();
+  });
+
+  it('should correctly change activeTab to register', () => {
+    //when
+    component.changeTab('register');
+
+    //then
+    expect(component.activeTab).toBe('register');
+    fixture.detectChanges();
+
+    const elements = fixture.debugElement.queryAll(By.css('.active'));
+    expect(elements.length).toBe(1);
+
+    let expectedDiv = elements[0].nativeElement;
+    expect(expectedDiv.id).toBe('signup');
+  });
+
+  it('should correctly change activeTab to forgot', () => {
+    //when
+    component.changeTab('forgot');
+
+    //then
+    expect(component.activeTab).toBe('forgot');
+    fixture.detectChanges();
+
+    const elements = fixture.debugElement.queryAll(By.css('.active'));
+    expect(elements.length).toBe(1);
+
+    let expectedDiv = elements[0].nativeElement;
+    expect(expectedDiv.id).toBe('forgot');
   });
 });
