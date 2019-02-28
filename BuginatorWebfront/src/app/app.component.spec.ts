e0a0cb8cc52cd615f6c4881dc447001df8bdf4b6
Arek Chrobot <chrobot.arkadiusz@gmail.com>
2019-02-28 22:46:04 +0100

    added test for app component and removed unusbed code section

--- BuginatorWebfront/src/app/app.component.spec.ts
+++ BuginatorWebfront/src/app/app.component.spec.ts
@@ -1,17 +1,42 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import {TestBed, async} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AppComponent} from './app.component';
+import {TranslateTestingModule} from "ngx-translate-testing";
+import {TranslateService} from "@ngx-translate/core";
+import {AuthComponent} from "./auth/auth.component";
+import {LoginComponent} from "./auth/login/login.component";
+import {PasswordResetComponent} from "./auth/password-reset/password-reset.component";
+import {RegisterComponent} from "./auth/register/register.component";
+import {ReactiveFormsModule} from "@angular/forms";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {CookieService} from "ngx-cookie-service";
 
 describe('AppComponent', () => {
+
+  let translateService: TranslateService;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        TranslateTestingModule
+          .withTranslations('en', require('../../src/assets/i18n/en.json')),
+        ReactiveFormsModule,
+        HttpClientTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        AuthComponent,
+        LoginComponent,
+        PasswordResetComponent,
+        RegisterComponent
       ],
+      providers: [
+        CookieService
+      ]
     }).compileComponents();
+
+    translateService = TestBed.get(TranslateService);
   }));
 
   it('should create the app', () => {
@@ -20,16 +45,11 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'buginator-webfront'`, () => {
+  it('should correctly set default lang and available langs', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('buginator-webfront');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to buginator-webfront!');
+    expect(translateService.getDefaultLang()).toBe('en');
+    expect(translateService.getLangs().length).toBe(2);
+    expect(translateService.getLangs()).toContain('en');
+    expect(translateService.getLangs()).toContain('pl');
   });
 });
